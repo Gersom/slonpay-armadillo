f9529a722c27f32a908d348e96c9f34600b9538a
Gersom SlonPay-Pc <gersomalaja@gmail.com>
2022-03-22 16:38:31 -0500

    add button open FAQ

--- src/layouts/FAQ.jsx
+++ src/layouts/FAQ.jsx
@@ -1,8 +1,9 @@
 // import React, { useState, Fragment } from "react";
-import React from "react";
+import React, {useState} from "react";
 
 export function FAQ() {
   // const [idTab, setIdTab] = useState('buy');
+  const [open, setOpen] = useState(true);
 
   return (
     <div className="FAQ o-wrapper">
@@ -12,22 +13,22 @@ export function FAQ() {
           Preguntas frecuentes</h2>
 
         <div className="FAQ__question-wrapper">
-          <h4 className="FAQ__question">
-            ¿Que es un NFT?</h4>
-          <p className="FAQ__response">
+          <button className="FAQ__question" onClick={() => setOpen(!open)}>
+            ¿Que es un NFT?</button>
+          <p className={open ? 'FAQ__response is-open' : 'FAQ__response'}>
             Un ejemplo de un token no fungible (NFT) sería una obra de arte. Si tienes un cuadro en casa, este no se consume al utilizarse y tampoco puede ser sustituido por otro cuadro. Una obra de arte no es equivalente a otra, y por lo tanto, no se pueden simplemente intercambiar.</p>
         </div>
         <div className="FAQ__question-wrapper">
-          <h4 className="FAQ__question">
-            ¿Tiene valor real un NFT?</h4>
+          <button className="FAQ__question">
+            ¿Tiene valor real un NFT?</button>
         </div>
         <div className="FAQ__question-wrapper">
-          <h4 className="FAQ__question">
-            ¿Como puedo comprar un Armadillo?</h4>
+          <button className="FAQ__question">
+            ¿Como puedo comprar un Armadillo?</button>
         </div>
         <div className="FAQ__question-wrapper">
-          <h4 className="FAQ__question">
-            ¿Luego podré venderla?</h4>
+          <button className="FAQ__question">
+            ¿Luego podré venderla?</button>
         </div>
 
       </div>
