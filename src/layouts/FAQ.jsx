772cb96f5816ffb629b5e613646c8dfe8de301de
Gersom SlonPay-Pc <gersomalaja@gmail.com>
2022-03-22 16:54:59 -0500

    add component Question in FAQ

--- src/layouts/FAQ.jsx
+++ src/layouts/FAQ.jsx
@@ -1,9 +1,8 @@
 // import React, { useState, Fragment } from "react";
-import React, {useState} from "react";
+import React from "react";
+import { Question } from "../components/Question";
 
 export function FAQ() {
-  // const [idTab, setIdTab] = useState('buy');
-  const [open, setOpen] = useState(true);
 
   return (
     <div className="FAQ o-wrapper">
@@ -12,24 +11,25 @@ export function FAQ() {
         <h2 className="FAQ__title">
           Preguntas frecuentes</h2>
 
-        <div className="FAQ__question-wrapper">
-          <button className="FAQ__question" onClick={() => setOpen(!open)}>
-            ¿Que es un NFT?</button>
-          <p className={open ? 'FAQ__response is-open' : 'FAQ__response'}>
-            Un ejemplo de un token no fungible (NFT) sería una obra de arte. Si tienes un cuadro en casa, este no se consume al utilizarse y tampoco puede ser sustituido por otro cuadro. Una obra de arte no es equivalente a otra, y por lo tanto, no se pueden simplemente intercambiar.</p>
-        </div>
-        <div className="FAQ__question-wrapper">
-          <button className="FAQ__question">
-            ¿Tiene valor real un NFT?</button>
-        </div>
-        <div className="FAQ__question-wrapper">
-          <button className="FAQ__question">
-            ¿Como puedo comprar un Armadillo?</button>
-        </div>
-        <div className="FAQ__question-wrapper">
-          <button className="FAQ__question">
-            ¿Luego podré venderla?</button>
-        </div>
+        <Question 
+        question='¿Que es un NFT?' 
+        response='Un ejemplo de un token no fungible (NFT) sería una obra de arte. Si tienes un cuadro en casa, este no se consume al utilizarse y tampoco puede ser sustituido por otro cuadro. Una obra de arte no es equivalente a otra, y por lo tanto, no se pueden simplemente intercambiar.'
+        ></Question>
+
+        <Question 
+        question='¿Tiene valor real un NFT?' 
+        response='Un ejemplo de un token no fungible (NFT) sería una obra de arte. Si tienes un cuadro en casa, este no se consume al utilizarse y tampoco puede ser sustituido por otro cuadro. Una obra de arte no es equivalente a otra, y por lo tanto, no se pueden simplemente intercambiar.'
+        ></Question>
+
+        <Question 
+        question='¿Como puedo comprar un Armadillo?' 
+        response='Un ejemplo de un token no fungible (NFT) sería una obra de arte. Si tienes un cuadro en casa, este no se consume al utilizarse y tampoco puede ser sustituido por otro cuadro. Una obra de arte no es equivalente a otra, y por lo tanto, no se pueden simplemente intercambiar.'
+        ></Question>
+
+        <Question 
+        question='¿Luego podré venderla?' 
+        response='Un ejemplo de un token no fungible (NFT) sería una obra de arte. Si tienes un cuadro en casa, este no se consume al utilizarse y tampoco puede ser sustituido por otro cuadro. Una obra de arte no es equivalente a otra, y por lo tanto, no se pueden simplemente intercambiar.'
+        ></Question>
 
       </div>
     </div>
